3fe88d863181b11470763d6e495f86a79c026c03
Lauana Daik <168572820+lauanadaik@users.noreply.github.com>
2024-06-04 23:17:46 +0100

    Add files via upload

---
+++ EstruturaSequencial_Exercicio1.java
@@ -0,0 +1,9 @@
+public class EstruturaSequencial_Exercicio1 {
+    public static void main(String[] args) {
+
+        /* Estrutura Sequencial - Exercício 1
+        Faça um Programa que mostre a mensagem "Alo mundo" na tela. */
+
+        System.out.println("Alô, mundo!");
+    }
+}
